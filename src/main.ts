08c8a1b1c17410ea860048adbad2632231105aaf
Dmytro Sukhariev <dmytro.sukhariev@convosight.com>
2022-09-18 16:03:15 +0300

    setup in one command

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*'],
+    origin: '*',
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.APP_PORT);
